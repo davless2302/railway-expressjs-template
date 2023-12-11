d96c3ecd0ce08f8a1da1f73ecce579fc02a7d4c8
davless2302 <david.f.gonzale@gmail.com>
2023-12-11 13:29:09 -0400

    Update equipmentController.js

--- src/controller/equipmentController.js
+++ src/controller/equipmentController.js
@@ -29,7 +29,42 @@ const getEquipment = async (req, res) => {
 };
 
 const CreateEquipment = async (req, res) => {
-  console.log(req.body);
+  const { chutoID, remolqueID, choferID } = req.body;
+
+  if (chutoID === "" || remolqueID === "" || choferID === "") {
+    res.status(400).json({ message: "Bad Request" });
+  } else {
+    try {
+      // Inicia la transacción
+
+      // Consulta para insertar en la tabla 'equipos'
+      const sqlEquipos =
+        "INSERT INTO equipos (idVehiculo, idChofer, idRemolque) VALUES (?, ?, ?)";
+      const valuesEquipos = [chutoID, choferID, remolqueID];
+      await pool.execute(sqlEquipos, valuesEquipos);
+
+      // Consultas para actualizar las tablas 'cars' y 'choferes'
+      const sqlUpdateCars = "UPDATE cars SET inUse = ? WHERE id = ?";
+      const valuesUpdateCars = ["Y", chutoID];
+      await pool.execute(sqlUpdateCars, valuesUpdateCars);
+
+      const valuesUpdateRemolque = ["Y", remolqueID];
+      await pool.execute(sqlUpdateCars, valuesUpdateRemolque);
+
+      const sqlUpdateChoferes = "UPDATE choferes SET inUse = ? WHERE id = ?";
+      const valuesUpdateChoferes = ["Y", choferID];
+      await pool.execute(sqlUpdateChoferes, valuesUpdateChoferes);
+
+      // Confirma la transacción
+
+      res.status(200).json({ message: "Equipo Asignado exitosamente" });
+    } catch (error) {
+      // Si hay un error, realiza un rollback para deshacer cualquier cambio
+
+      console.error(error);
+      res.status(500).json({ message: "Internal Error" });
+    }
+  }
 };
 
 export const methods = {
