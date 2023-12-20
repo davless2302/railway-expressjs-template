eb7ecd75b90965a0d57f64a0f6189ebab7b18465
davless2302 <david.f.gonzale@gmail.com>
2023-12-19 20:08:40 -0400

    Update GuideController.js

--- src/controller/GuideController.js
+++ src/controller/GuideController.js
@@ -186,219 +186,217 @@ const getGuide = async (req, res) => {
 const UpdateGuide = async (req, res) => {
   const { estado } = req.body;
   if (estado === "P") {
-    console.log(req.body);
     const { nGuia, fechaSalida } = req.body;
     const files = req.files ? req.files : null;
-    // try {
-    //   const deleteFiles = async (files) => {
-    //     if (files && files.length > 0) {
-    //       for (let file of files) {
-    //         const filePath = path.join(
-    //           __dirname,
-    //           "../static/images/guide",
-    //           file.filename
-    //         );
+    try {
+      const deleteFiles = async (files) => {
+        if (files && files.length > 0) {
+          for (let file of files) {
+            const filePath = path.join(
+              __dirname,
+              "../static/images/guide",
+              file.filename
+            );
 
-    //         try {
-    //           await unlink(filePath);
-    //           console.log(`Archivo eliminado: ${filePath}`);
-    //         } catch (unlinkError) {
-    //           console.error(
-    //             `Error al eliminar el archivo: ${filePath}`,
-    //             unlinkError
-    //           );
-    //         }
-    //       }
-    //     }
-    //   };
+            try {
+              await unlink(filePath);
+              console.log(`Archivo eliminado: ${filePath}`);
+            } catch (unlinkError) {
+              console.error(
+                `Error al eliminar el archivo: ${filePath}`,
+                unlinkError
+              );
+            }
+          }
+        }
+      };
 
-    //   if (!nGuia || !fechaSalida) {
-    //     if (files) {
-    //       await deleteFiles(files);
-    //     }
-    //     return res.status(400).json({ message: "Solicitud incorrecta" });
-    //   }
+      if (!nGuia || !fechaSalida) {
+        if (files) {
+          await deleteFiles(files);
+        }
+        return res.status(400).json({ message: "Solicitud incorrecta" });
+      }
 
-    //   // Obtener los documentos actuales de la base de datos
-    //   const selectSql = "SELECT documents FROM carga WHERE nGuia = ?";
-    //   const [currentDocuments] = await pool.query(selectSql, [nGuia]);
+      // Obtener los documentos actuales de la base de datos
+      const selectSql = "SELECT documents FROM carga WHERE nGuia = ?";
+      const [currentDocuments] = await pool.query(selectSql, [nGuia]);
 
-    //   let combinedDocuments = currentDocuments[0].documents;
+      let combinedDocuments = currentDocuments[0].documents;
 
-    //   // Agregar nuevos documentos solo si hay archivos
-    //   if (files && files.length > 0) {
-    //     combinedDocuments = combinedDocuments
-    //       ? combinedDocuments.concat(
-    //           ",",
-    //           files.map((file) => file.filename)
-    //         )
-    //       : files.map((file) => file.filename).join(",");
-    //   }
-    //   // Actualizar la base de datos con los documentos combinados
-    //   const updateSql =
-    //     "UPDATE carga SET fechaSalida = ?, documents = ?, estado = 'O' WHERE nGuia = ?";
-    //   await pool.query(updateSql, [fechaSalida, combinedDocuments, nGuia]);
+      // Agregar nuevos documentos solo si hay archivos
+      if (files && files.length > 0) {
+        combinedDocuments = combinedDocuments
+          ? combinedDocuments.concat(
+              ",",
+              files.map((file) => file.filename)
+            )
+          : files.map((file) => file.filename).join(",");
+      }
+      // Actualizar la base de datos con los documentos combinados
+      const updateSql =
+        "UPDATE carga SET fechaSalida = ?, documents = ?, estado = 'O' WHERE nGuia = ?";
+      await pool.query(updateSql, [fechaSalida, combinedDocuments, nGuia]);
 
-    //   res.status(200).json({ message: "Guía actualizada correctamente" });
-    // } catch (error) {
-    //   console.log(error);
-    //   if (files) {
-    //     await deleteFiles(files);
-    //   }
-    //   return res.status(500).json({ message: "Internal Server Error" });
-    // }
+      res.status(200).json({ message: "Guía actualizada correctamente" });
+    } catch (error) {
+      console.log(error);
+      if (files) {
+        await deleteFiles(files);
+      }
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  } else if (estado === "O") {
+    const { nGuia, fechaLlegada, kmDestino, CombustibleDestino } = req.body;
+    const files = req.files || null;
+
+    const deleteFiles = async (files) => {
+      if (files && files.length > 0) {
+        for (let file of files) {
+          const filePath = path.join(
+            __dirname,
+            "../static/images/guide",
+            file.filename
+          );
+
+          try {
+            await unlink(filePath);
+            console.log(`Archivo eliminado: ${filePath}`);
+          } catch (unlinkError) {
+            console.error(
+              `Error al eliminar el archivo: ${filePath}`,
+              unlinkError
+            );
+          }
+        }
+      }
+    };
+
+    try {
+      if (!nGuia || !fechaLlegada || !kmDestino || !CombustibleDestino) {
+        if (files) {
+          await deleteFiles(files);
+        }
+        return res.status(400).json({ message: "Solicitud incorrecta" });
+      }
+
+      // Obtener documentos y ID de equipo actuales de la base de datos
+      const selectSql = "SELECT documents, idEquipo FROM carga WHERE nGuia = ?";
+      const [currentData] = await pool.query(selectSql, [nGuia]);
+
+      let combinedDocuments = currentData[0].documents;
+
+      if (files && files.length > 0) {
+        combinedDocuments = combinedDocuments
+          ? combinedDocuments.concat(
+              ",",
+              files.map((file) => file.filename)
+            )
+          : files.map((file) => file.filename).join(",");
+      }
+
+      // Obtener el ID del vehículo asociado al equipo
+      const query = "SELECT idVehiculo FROM equipos WHERE id = ?";
+      const [result] = await pool.query(query, [currentData[0].idEquipo]);
+
+      // Obtener el kilometraje actual del vehículo
+      const query2 = "SELECT km FROM cars WHERE id = ?";
+      const [result2] = await pool.query(query2, [result[0].idVehiculo]);
+
+      if (kmDestino < result2[0].km) {
+        return res
+          .status(400)
+          .json({ message: "El Kilometraje es Menor al Ingresado en Sistema" });
+      }
+
+      // Actualizar la base de datos con los documentos y otros datos
+      const updateSql =
+        "UPDATE carga SET fechaLlegada = ?, kmEquipoDestino = ?, CombustibleDestino = ?, documents = ?, estado = 'C' WHERE nGuia = ?";
+      await pool.query(updateSql, [
+        fechaLlegada,
+        kmDestino,
+        CombustibleDestino,
+        combinedDocuments,
+        nGuia,
+      ]);
+
+      // Actualizar el kilometraje del vehículo
+      const updateSql2 = "UPDATE cars SET km = ?  WHERE id = ?";
+      await pool.query(updateSql2, [kmDestino, result[0].idVehiculo]);
+
+      res.status(200).json({ message: "Guía actualizada correctamente" });
+    } catch (error) {
+      console.log(error);
+
+      // Eliminar archivos en caso de error
+      if (files) {
+        await deleteFiles(files);
+      }
+
+      return res.status(500).json({ message: "Error interno del servidor" });
+    }
+  } else if (estado === "C") {
+    const { nGuia } = req.body;
+    const files = req.files ? req.files : null;
+    try {
+      const deleteFiles = async (files) => {
+        if (files && files.length > 0) {
+          for (let file of files) {
+            const filePath = path.join(
+              __dirname,
+              "../static/images/guide",
+              file.filename
+            );
+
+            try {
+              await unlink(filePath);
+              console.log(`Archivo eliminado: ${filePath}`);
+            } catch (unlinkError) {
+              console.error(
+                `Error al eliminar el archivo: ${filePath}`,
+                unlinkError
+              );
+            }
+          }
+        }
+      };
+
+      if (!nGuia || !estado) {
+        if (files) {
+          await deleteFiles(files);
+        }
+        return res.status(400).json({ message: "Solicitud incorrecta" });
+      }
+
+      // Obtener los documentos actuales de la base de datos
+      const selectSql = "SELECT documents FROM carga WHERE nGuia = ?";
+      const [currentDocuments] = await pool.query(selectSql, [nGuia]);
+
+      let combinedDocuments = currentDocuments[0].documents;
+
+      // Agregar nuevos documentos solo si hay archivos
+      if (files && files.length > 0) {
+        combinedDocuments = combinedDocuments
+          ? combinedDocuments.concat(
+              ",",
+              files.map((file) => file.filename)
+            )
+          : files.map((file) => file.filename).join(",");
+      }
+      // Actualizar la base de datos con los documentos combinados
+      const updateSql =
+        "UPDATE carga SET documents = ?, estado = 'PC' WHERE nGuia = ?";
+      await pool.query(updateSql, [combinedDocuments, nGuia]);
+
+      res.status(200).json({ message: "Guía actualizada correctamente" });
+    } catch (error) {
+      console.log(error);
+      if (files) {
+        await deleteFiles(files);
+      }
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
   }
-  // else if (estado === "O") {
-  //   const { nGuia, fechaLlegada, kmDestino, CombustibleDestino } = req.body;
-  //   const files = req.files || null;
-
-  //   const deleteFiles = async (files) => {
-  //     if (files && files.length > 0) {
-  //       for (let file of files) {
-  //         const filePath = path.join(
-  //           __dirname,
-  //           "../static/images/guide",
-  //           file.filename
-  //         );
-
-  //         try {
-  //           await unlink(filePath);
-  //           console.log(`Archivo eliminado: ${filePath}`);
-  //         } catch (unlinkError) {
-  //           console.error(
-  //             `Error al eliminar el archivo: ${filePath}`,
-  //             unlinkError
-  //           );
-  //         }
-  //       }
-  //     }
-  //   };
-
-  //   try {
-  //     if (!nGuia || !fechaLlegada || !kmDestino || !CombustibleDestino) {
-  //       if (files) {
-  //         await deleteFiles(files);
-  //       }
-  //       return res.status(400).json({ message: "Solicitud incorrecta" });
-  //     }
-
-  //     // Obtener documentos y ID de equipo actuales de la base de datos
-  //     const selectSql = "SELECT documents, idEquipo FROM carga WHERE nGuia = ?";
-  //     const [currentData] = await pool.query(selectSql, [nGuia]);
-
-  //     let combinedDocuments = currentData[0].documents;
-
-  //     if (files && files.length > 0) {
-  //       combinedDocuments = combinedDocuments
-  //         ? combinedDocuments.concat(
-  //             ",",
-  //             files.map((file) => file.filename)
-  //           )
-  //         : files.map((file) => file.filename).join(",");
-  //     }
-
-  //     // Obtener el ID del vehículo asociado al equipo
-  //     const query = "SELECT idVehiculo FROM equipos WHERE id = ?";
-  //     const [result] = await pool.query(query, [currentData[0].idEquipo]);
-
-  //     // Obtener el kilometraje actual del vehículo
-  //     const query2 = "SELECT km FROM cars WHERE id = ?";
-  //     const [result2] = await pool.query(query2, [result[0].idVehiculo]);
-
-  //     if (kmDestino < result2[0].km) {
-  //       return res
-  //         .status(400)
-  //         .json({ message: "El Kilometraje es Menor al Ingresado en Sistema" });
-  //     }
-
-  //     // Actualizar la base de datos con los documentos y otros datos
-  //     const updateSql =
-  //       "UPDATE carga SET fechaLlegada = ?, kmEquipoDestino = ?, CombustibleDestino = ?, documents = ?, estado = 'C' WHERE nGuia = ?";
-  //     await pool.query(updateSql, [
-  //       fechaLlegada,
-  //       kmDestino,
-  //       CombustibleDestino,
-  //       combinedDocuments,
-  //       nGuia,
-  //     ]);
-
-  //     // Actualizar el kilometraje del vehículo
-  //     const updateSql2 = "UPDATE cars SET km = ?  WHERE id = ?";
-  //     await pool.query(updateSql2, [kmDestino, result[0].idVehiculo]);
-
-  //     res.status(200).json({ message: "Guía actualizada correctamente" });
-  //   } catch (error) {
-  //     console.log(error);
-
-  //     // Eliminar archivos en caso de error
-  //     if (files) {
-  //       await deleteFiles(files);
-  //     }
-
-  //     return res.status(500).json({ message: "Error interno del servidor" });
-  //   }
-  // } else if (estado === "C") {
-  //   const { nGuia } = req.body;
-  //   const files = req.files ? req.files : null;
-  //   try {
-  //     const deleteFiles = async (files) => {
-  //       if (files && files.length > 0) {
-  //         for (let file of files) {
-  //           const filePath = path.join(
-  //             __dirname,
-  //             "../static/images/guide",
-  //             file.filename
-  //           );
-
-  //           try {
-  //             await unlink(filePath);
-  //             console.log(`Archivo eliminado: ${filePath}`);
-  //           } catch (unlinkError) {
-  //             console.error(
-  //               `Error al eliminar el archivo: ${filePath}`,
-  //               unlinkError
-  //             );
-  //           }
-  //         }
-  //       }
-  //     };
-
-  //     if (!nGuia || !estado) {
-  //       if (files) {
-  //         await deleteFiles(files);
-  //       }
-  //       return res.status(400).json({ message: "Solicitud incorrecta" });
-  //     }
-
-  //     // Obtener los documentos actuales de la base de datos
-  //     const selectSql = "SELECT documents FROM carga WHERE nGuia = ?";
-  //     const [currentDocuments] = await pool.query(selectSql, [nGuia]);
-
-  //     let combinedDocuments = currentDocuments[0].documents;
-
-  //     // Agregar nuevos documentos solo si hay archivos
-  //     if (files && files.length > 0) {
-  //       combinedDocuments = combinedDocuments
-  //         ? combinedDocuments.concat(
-  //             ",",
-  //             files.map((file) => file.filename)
-  //           )
-  //         : files.map((file) => file.filename).join(",");
-  //     }
-  //     // Actualizar la base de datos con los documentos combinados
-  //     const updateSql =
-  //       "UPDATE carga SET documents = ?, estado = 'PC' WHERE nGuia = ?";
-  //     await pool.query(updateSql, [combinedDocuments, nGuia]);
-
-  //     res.status(200).json({ message: "Guía actualizada correctamente" });
-  //   } catch (error) {
-  //     console.log(error);
-  //     if (files) {
-  //       await deleteFiles(files);
-  //     }
-  //     return res.status(500).json({ message: "Internal Server Error" });
-  //   }
-  // }
 };
 
 export const methods = {
